f32a7fbe05f0464337e6a254efcafccc56f88a26
Alexandr Kurevlev <alexandrkurevlev@gmail.com>
2026-09-08 06:59:41 +0300

    Add rules

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+var DefaultRules = map[string]string{
+	".jpg":  "Images",
+	".jpeg": "Images",
+	".png":  "Images",
+	".pdf":  "Documents",
+	".doc":  "Documents",
+	".docx": "Documents",
+	".txt":  "Documents",
+	".mp3":  "Music",
+	".wav":  "Music",
+	".mp4":  "Video",
+	".avi":  "Video",
+	".zip":  "Archives",
+	".rar":  "Archives",
+}
+
+func main() {
+	for k, v := range DefaultRules {
+		fmt.Println("Расширение:", k, "->", "Папка:", v)
+	}
+}
