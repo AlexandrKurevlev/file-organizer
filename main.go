6393860e8188f0b974f3030bea3459bbc7055f6d
Alexandr Kurevlev <alexandrkurevlev@gmail.com>
2026-09-10 15:34:37 +0300

    Add func to move file

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 type FileOrganizer struct {
@@ -59,6 +62,29 @@ func (fo *FileOrganizer) Close() error {
 	}
 
 	return fo.logFile.Close()
+}
+
+func (fo *FileOrganizer) moveFile(sourcePath, targetDir string) error {
+	targetPath := filepath.Join(fo.sourceDir, targetDir)
+	err := os.MkdirAll(targetPath, 0750)
+	if err != nil {
+		return fmt.Errorf("ошибка создания новой директории: %q: %q", targetPath, err)
+	}
+
+	filename := filepath.Base(sourcePath)
+	_, err = os.Stat(filepath.Join(targetPath, filename))
+	if err == nil {
+		filename = filename[:len(filename)-len(filepath.Ext(filename))] + "_" + time.Now().Format("2006-01-02_15-04-05") + filepath.Ext(filename)
+	} else if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("ошибка проверки существования файла: %q: %q", targetPath, err)
+	}
+
+	targetPath = filepath.Join(targetPath, filename)
+	err = os.Rename(sourcePath, targetPath)
+	if err != nil {
+		return fmt.Errorf("ошибка при перемещении файла из %q в %q: %q", sourcePath, targetPath, err)
+	}
+	return nil
 }
 
 var DefaultRules = map[string]string{
